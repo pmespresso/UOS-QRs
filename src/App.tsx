fe371d7c8b99ce78b3cb7c917fcf8119e3d3eac0
YJ <yjkimjunior@gmail.com>
2019-07-29 11:42:25 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { QrDisplayAddress, QrDisplayPayload } from './ui-qr/src';
 
@@ -7,12 +7,19 @@ import './legacy.png';
 import './App.css';
 
 const App: React.FC = () => {
+  const [payload, setPayload] = useState('THIS IS SPARTA!');
+
+  const handleChangePayload = ({ target: { value }}: React.ChangeEvent<HTMLInputElement>) => {
+    setPayload(value);
+  }
+
   return (
     <React.Fragment>
       <h1>UOS Testing QRs</h1>
       <div style={{ display: 'flex' }}>
         <div style={{ flex: 1, margin: '10px' }}>
           <h3>Substrate UOS Address</h3>
+          <label>Address: </label><p> 5HbgaJEuVN5qGbkhgtuDQANivSWwHXWsC2erP1SQUXgciTVq </p>
           <QrDisplayAddress
             address={'5HbgaJEuVN5qGbkhgtuDQANivSWwHXWsC2erP1SQUXgciTVq'}
             style={{ width: '300px', height: '300px' }} />
@@ -20,10 +27,12 @@ const App: React.FC = () => {
 
         <div style={{ flex: 1, margin: '10px' }}>
           <h3>Substrate UOS Payload</h3>
+          <label>Address: </label><p> 5HbgaJEuVN5qGbkhgtuDQANivSWwHXWsC2erP1SQUXgciTVq </p>
+          <label>Payload: </label><input onChange={handleChangePayload} type='text' value={payload}></input>
           <QrDisplayPayload
-            address={'5HbgaJEuVN5qGbkhgtuDQANivSWwHXWsC2erP1SQUXgciTVq'}
-            payload={new TextEncoder().encode('Subliminal Text Message')}
-            style={{ width: '300px', height: '300px' }} />
+              address={'5HbgaJEuVN5qGbkhgtuDQANivSWwHXWsC2erP1SQUXgciTVq'}
+              payload={new TextEncoder().encode(payload)}
+              style={{ width: '300px', height: '300px' }} />
         </div>
 
         <div style={{ flex: 1, margin: '10px' }}>
@@ -36,7 +45,6 @@ const App: React.FC = () => {
           <img src='legacy.png' alt='legacy' />
         </div>
       </div>
-      
     </React.Fragment>
   );
 }
